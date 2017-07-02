b6ea20b1f3b8ba8e564895fab68cfed10af5c94a
Mateusz Kasprzak <kasprzak.mat@gmail.com>
2017-07-02 18:57:47 +0200

    Use List component in core

--- core/index.js
+++ core/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import {
-  View
+  View,
+  StyleSheet
 } from 'react-native';
+import List from './containers/List';
 
 export default () => (
-<View>
-
+  <View style={styles.container}>
+    <List />
   </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f6f9fd',
+    height: '100%',
+    flex: 1,
+    padding: 20,
+  },
+});
